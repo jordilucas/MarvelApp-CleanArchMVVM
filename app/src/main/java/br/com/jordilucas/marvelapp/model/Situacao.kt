c5c301017e7ab5e891bc266ce6c8985a78a0f549
Jordi <jordilucas3@gmail.com>
2021-05-06 23:26:27 -0300

    Classe para situacao do request

---
+++ app/src/main/java/br/com/jordilucas/marvelapp/model/Situacao.kt
@@ -0,0 +1,14 @@
+package br.com.jordilucas.marvelapp.model
+
+object Situacao {
+
+    const val Missing_API_Key = 409
+    const val Missing_Hash = 409
+    const val Missing_Timestamp = 409
+    const val Invalid_Referer = 401
+    const val Missing_Invalid_Hash = 401
+    const val Missing_Method_Not_Allowed = 405
+    const val Missing_Forbidden = 403
+    const val OK = 200
+
+}
