3b03feb78be1ddcfea6e7ea0b6c4b4983d539295
Jordi <jordilucas3@gmail.com>
2021-05-07 13:20:52 -0300

    configuracao navigation

--- app/src/main/java/br/com/jordilucas/marvelapp/MainActivity.kt
+++ app/src/main/java/br/com/jordilucas/marvelapp/MainActivity.kt
@@ -2,10 +2,20 @@ package br.com.jordilucas.marvelapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.NavHostFragment
+import br.com.jordilucas.marvelapp.model.Personagens
+import br.com.jordilucas.marvelapp.ui.listapersonagens.PersonagensViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        navController.currentDestination
     }
 }
