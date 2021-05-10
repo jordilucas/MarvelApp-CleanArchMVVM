d1fa70abe54d05dc528713d68bf94e839f01cf75
Jordi <jordilucas3@gmail.com>
2021-05-09 22:23:48 -0300

    Classe modelo para receber os dados de revista do servico da marvel

--- app/src/main/java/br/com/jordilucas/marvelapp/model/Revistas.kt
+++ app/src/main/java/br/com/jordilucas/marvelapp/model/Revistas.kt
@@ -1,8 +1,10 @@
 package br.com.jordilucas.marvelapp.model
 
+import java.io.Serializable
+
 data class Revistas(
      val title: String,
      val description: String,
      val prices: List<prices>,
      val images: List<images>
-)
+) : Serializable
